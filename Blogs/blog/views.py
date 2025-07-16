7afb97214cc7022818ead7ce84e4dff35abe18ee
Sanjay <sanjaydevarajworks@gmail.com>
2025-07-16 23:30:53 +0530

    Self urls

--- Blogs/blog/views.py
+++ Blogs/blog/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.urls import reverse
+from .models import Post
 
 # Create your views here.
 def index(request):
     blog_title = "Latest Posts"
-    return render(request,"index.html", {'blog_title' : blog_title})
+    posts = [
+        {'id':1, 'title': 'Post 1', 'Content': 'Content of Post 1'},
+        {'id':2, 'title': 'Post 2', 'Content': 'Content of Post 2'},
+        {'id':3, 'title': 'Post 3', 'Content': 'Content of Post 3'},
+        {'id':4, 'title': 'Post 4', 'Content': 'Content of Post 4'}
+
+    ]
+
+
+    return render(request,"index.html", {'blog_title': blog_title, 'posts': posts})
 
 def detail(request, post_id):
-    return render(request,"detail.html")
+    post = get_object_or_404(Post, id=post_id)
+    return render(request,"detail.html", {'post':post})
 
 def old_url_redirect(request):
     return redirect(reverse("blog:new_page_url"))
